14c31e8037324938a3c8e14b2d7e50b731837b22
Will in China <email.will.in.china@gmail.com>
2018-07-02 19:10:25 +0800

    git scripts

--- src/blox-app.js
+++ src/blox-app.js
@@ -16,20 +16,11 @@ class BloxApp extends PolymerElement {
       <blox-keypair id="keypair"></blox-keypair>
       <div class="container">
         <h1>{{title}}</h1>
-        <input type="text" class="text" placeholder="EOS Public Key" id="publicKey">
-        <input type="text" class="text" placeholder="EOS Private Key" id="privateKey">
-        <button class="btn-main" on-click="makeKeyPair">Make Key Pair</button>
       </div>
     `;
   }
 
-  makeKeyPair(){
-    this.$.keypair.generate()
-    .then((keypair) => {
-      this.shadowRoot.querySelector('#publicKey').value = keypair[0].publicKey;
-      this.shadowRoot.querySelector('#privateKey').value = keypair[0].privateKey;
-    })
-  }
+
 
   static get properties() {
     return {
